0db797dc39d7de96eea0c1cb5497316bfb60c6ad
Alberto Fernández Gómez <afernandezgom@gmail.com>
2020-04-04 17:19:06 +0200

    fix: fixes contract lint

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,23 +5,24 @@ contract Lottery {
   address public manager;
   address[] public players;
 
-  constructor() {
+  function Lottery() {
     manager = msg.sender;
   }
 
   function enter() public payable {
-    require(msg.value > .01 ether, 'Value sent must be greater than 0.1 ether');
+    require(msg.value > .01 ether);
 
     players.push(msg.sender);
   }
 
   function random() private view returns(uint) {
-      eturn uint(keccak256(block.difficulty, now, players));
+      return uint(keccak256(block.difficulty, now, players));
   }
 
   function pickWinner() public restricted {
     uint index = random() % players.length;
     players[index].transfer(this.balance);
+    lastWinner = players[index];
     players = new address[](0);
   }
 
@@ -30,7 +31,7 @@ contract Lottery {
   }
 
   modifier restricted() {
-    require(msg.sender == manager, 'Method not allowed');
+    require(msg.sender == manager);
     _;
   }
 }
